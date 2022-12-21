2b5f034e4ea0ddfe1def9a7ed21f672146577a80
Fredrick-245 <ndemofredrick245@gmail.com>
2022-12-21 11:29:55 +0800

    Initial commit

---
+++ other.js
@@ -0,0 +1,11 @@
+// Setting local storage
+// Used for small amounts of data,shouldnt be used to store large amount of data
+/*
+const setLocalStorage = function () {
+  localStorage.setItem('workouts', JSON.stringify(this.#workout));
+};
+const getLocalData=function(){
+ cost data=JSON.parse( localStorage.getItem('workouts'));
+ if(!data) return
+}
+*/
